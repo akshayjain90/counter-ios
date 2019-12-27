95a496f29127a3a7693052b9904e9c02d817d5b0
pragyagoel04 <pragyagoel04@gmail.com>
2019-12-26 16:12:21 -0800

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Pragya Goel on 12/26/19.
+//  Copyright © 2019 GenericGenome. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
